e56807a932ece1d9ea07ce71d5f59f5241da7a49
nadiafarhanii <nadiafarhani25@gmail.com>
2021-04-04 18:46:10 +0700

    Membuat file form, index.php

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Data Pengiriman Barang</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="jquery/jquery-3.4.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+    
+    <div class="container p-3 my-3 border">
+    <h1 class="text-center">Form Pengiriman Barang</h1>
+    <?php
+    //Include file koneksi, untuk koneksikan ke database
+    include "koneksi.php";
+    
+    //Fungsi untuk mencegah inputan karakter yang tidak sesuai
+    function input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    //Cek apakah ada kiriman form dari method post
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $nama=input($_POST["nama"]);
+        $nik=input($_POST["nik"]);
+        $jenis_barang=input($_POST["jenis_barang"]);
+        $tanggal_pengiriman=input($_POST["tanggal_pengiriman"]);
+        $kewarganegaraan=input($_POST["kewarganegaraan"]);
+        $email=input($_POST["email"]);
+        $no_telp=input($_POST["no_telp"]);
+        $alamat=input($_POST["alamat"]);
+        $kode_pos=input($_POST["kode_pos"]);
+        $provinsi=input($_POST["provinsi"]);
+        $kabupaten=input($_POST["kabupaten"]);
+        $kecamatan=input($_POST["kecamatan"]);
+        
+
+        //Query input menginput data kedalam tabel pendaftaraan
+        $sql="insert into pendaftaran (nama,nik,jenis_barang,tanggal_pengiriman,kewarganegaraan,email,no_telp,alamat,kode_pos,provinsi,kabupaten,kecamatan) values
+		('$nama','$nik','$jenis_barang','$tanggal_pengiriman','$kewarganegaraan','$email','$no_telp','$alamat','$kode_pos','$provinsi','$kabupaten','$kecamatan')";
+
+        //Mengeksekusi/menjalankan query diatas
+        $hasil=mysqli_query($kon,$sql);
+
+        //Kondisi apakah berhasil atau tidak dalam mengeksekusi query diatas
+        if ($hasil) { 
+            echo "<div class='alert alert-success'> Selamat $nama anda telah berhasil mendaftar.</div>"; 
+        }
+        else {
+            echo "<div class='alert alert-danger'> Pendaftaraan Gagal.</div>";
+        }
+    }
+    ?>
+        <form id="form" method="post">
+            <div class="alert alert-primary">
+                <strong>Data Diri</strong>
+            </div>
+            <div class="row">
+                <div class="col-sm-7">
+
+                    <div class="form-group">
+                        <label>Nama Lengkap:</label>
+                        <input type="text" name="nama" class="form-control" placeholder="Masukan Nama Lengkap">
+                    </div>
+                </div>
+                <div class="col-sm-5">
+                    <div class="form-group">
+                        <label>Nomor Identitas (NIK):</label>
+                        <input type="text" name="nik" class="form-control" placeholder="Masukan Nomor NIK">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="form-group">
+                        <label>Jenis Barang:</label>
+                        <input type="text" name="jenis_barang" class="form-control" placeholder="Masukan jenis barang">
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-group">
+                        <label>Tanggal Pengiriman:</label>
+                        <input type="date" name="tanggal_pengiriman" class="form-control">
+                    </div>
+                </div>
+                
+            </div>
+
+            <div class="row">
+
+                <div class="col-sm-4">
+                    <div class="form-group">
+                        <label>Kewarganegaraan:</label>
+                        <select class="form-control" name="kewarganegaraan">
+                            <option>Pilih</option>
+                            <option value="WNI">Warga Negara Indonesia</option>
+                            <option value="WNA">Warga Negara Asing</option>
+                        </select>
+                    </div>
+                </div>
+               
+            </div>
+
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="form-group">
+                        <label>Email:</label>
+                        <input type="email" name="email" class="form-control" placeholder="Masukan Email">
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="form-group">
+                        <label>No Telp:</label>
+                        <input type="text" name="no_telp" class="form-control" placeholder="Masukan No Telp">
+                    </div>
+                </div>
+            </div>
+            <div class="alert alert-primary">
+                <strong>Alamat Tujuan</strong>
+            </div>
+            <div class="row">
+                <div class="col-sm-5">
+                    <div class="form-group">
+                        <label>Alamat:</label>
+                        <textarea class="form-control" name="alamat" rows="2" id="alamat"></textarea>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="form-group">
+                        <label>Kode Pos:</label>
+                        <input type="text" name="kode_pos" class="form-control" placeholder="Kode Pos">
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="form-group">
+                        <label>Provinsi:</label>
+                        <select class="form-control" name="provinsi" id="provinsi">
+                            <?php
+                            include "koneksi.php";
+                            //Perintah sql untuk menampilkan semua data pada tabel provinsi
+                            $sql="select * from provinsi";
+                            $hasil=mysqli_query($kon,$sql);
+                            while ($data = mysqli_fetch_array($hasil)) {
+                                ?>
+                            <option value="<?php echo $data['id_prov'];?>"><?php echo $data['nama'];?></option>
+                            <?php
+                                }
+                        ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="form-group">
+                        <label>Kabupaten:</label>
+                        <select class="form-control" name="kabupaten" id="kabupaten">
+                            <!-- Kabupaten akan diload menggunakan ajax, dan ditampilkan disini -->
+                        </select>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="form-group">
+                        <label>Kecamatan:</label>
+                        <select class="form-control" name="kecamatan" id="kecamatan">
+                            <!-- Kecamatan akan diload menggunakan ajax, dan ditampilkan disini -->
+                        </select>
+                    </div>
+                </div>
+
+            </div>
+            <script>
+            $("#provinsi").change(function() {
+                // variabel dari nilai combo provinsi
+                var id_provinsi = $("#provinsi").val();
+
+                // Menggunakan ajax untuk mengirim dan dan menerima data dari server
+                $.ajax({
+                    type: "POST",
+                    dataType: "html",
+                    url: "ambil-data.php",
+                    data: "provinsi=" + id_provinsi,
+                    success: function(data) {
+                        $("#kabupaten").html(data);
+                    }
+                });
+            });
+
+            $("#kabupaten").change(function() {
+                // variabel dari nilai combo box kabupaten
+                var id_kabupaten = $("#kabupaten").val();
+
+                // Menggunakan ajax untuk mengirim dan dan menerima data dari server
+                $.ajax({
+                    type: "POST",
+                    dataType: "html",
+                    url: "ambil-data.php",
+                    data: "kabupaten=" + id_kabupaten,
+                    success: function(data) {
+                        $("#kecamatan").html(data);
+                    }
+                });
+            });
+            </script>
+           
+            <div class="row">
+                <div class="col-sm-4">
+                    <button type="submit" name="Submit" id="Submit" class="btn btn-primary">Daftar</button>
+                    <button type="reset" class="btn btn-secondary">Reset</button>
+                </div>
+
+            </div>
+        </form>
+    </div>
+</body>
+</html>
